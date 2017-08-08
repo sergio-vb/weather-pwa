58a5d2372e9c1fd4079a3c79c13dcbfed91732f4
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-08-08 12:16:37 -0600

    Adding validation to avoid overwriting data with older data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -105,9 +105,16 @@
       app.container.appendChild(card);
       app.visibleCards[data.key] = card;
     }
+    
+    // Verify data is newer than what we already have, if not, bail.
+    var dateElem = card.querySelector('.date');
+    if (dateElem.getAttribute('data-dt') >= data.currently.time) {
+      return;
+    }
+
+    dateElem.setAttribute('data-dt', data.currently.time);
+    dateElem.textContent = new Date(data.currently.time * 1000);
     card.querySelector('.description').textContent = data.currently.summary;
-    card.querySelector('.date').textContent =
-      new Date(data.currently.time * 1000);
     card.querySelector('.current .icon').classList.add(data.currently.icon);
     card.querySelector('.current .temperature .value').textContent =
       Math.round(data.currently.temperature);
